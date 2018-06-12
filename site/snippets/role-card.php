22a9b83ee5ef29c3c5cf7df67f2cb33be935019d
Arthur Schmitt <tart2000design@gmail.com>
2018-06-12 17:57:15 +0200

    better subdivision in page 'cercles'

--- site/snippets/role-card.php
+++ site/snippets/role-card.php
@@ -1,12 +1,17 @@
 <h5>
-	<?= $r->title() ?>
+	<a href="<?= $r->url() ?>">
+		<?= $r->title() ?>
+	</a>
 	<?php if ($r->lead() != '') : ?>
 		<?php $lead = $r->lead() ?>
 		<?php $lead = $site->user($lead) ?>
-		<i class="fa fa-user"></i> 
-		<?= $site->user($lead)->firstName().' '.$site->user($lead)->lastName() ?>
+		<i class="fa fa-user"></i> lead : 
+		<a href="<?php echo $site->url().'/staff/'.$site->user($lead)->username() ?>">
+			<?= $site->user($lead)->firstName().' '.$site->user($lead)->lastName() ?>
+		</a>
 	<?php endif ?>
 </h5>
+
 <?php if ($r->hasChildren()) : ?>
 	<ul>
 		<?php foreach ($r->children() as $role) : ?>
@@ -16,9 +21,18 @@
 <?php endif ?>
 
 <?php if ($r->people() != '') : ?>
-	<?php $staff = $r->people() ?>
-	<?php foreach ($staff->toStructure() as $user) : ?>
-		<?php $user = $site->user($user) ?>
-		<?= $user ?>
-	<?php endforeach ?>
+	<ul>
+		<?php $staff = $r->people() ?>
+		<?php foreach ($staff->toStructure() as $user) : ?>
+			<?php $theuser = $site->user($user->staff()) ?>
+			<li>
+				<a href="<?php echo $site->url().'/staff/'.$theuser->username() ?>">
+					<?= $theuser->firstName().' '.$theuser->lastName() ?>
+				</a>
+				<?php if ($user->job() != '') : ?>
+					- <?= $user->job() ?>
+				<?php endif ?>
+			</li>
+		<?php endforeach ?>
+	</ul>
 <?php endif ?>
